a30cde18c9d02648fd89ecb1aff2503601078c95
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-03-27 12:26:56 +0530

    CREATE: main ui colors and theme created

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xFFEF1B48);
+const bubbleLight = Color(0xFFE8E8E8);
+const bubbleDark = Color(0xFF262629);
+const appBarDark = Color(0xFF111111);
+const activeUsersDark = Color(0xFF3B3B3B);
+const indicatorBubble = Color(0xFF39B54A);
+const iconLight = Color(0xFF999999);
