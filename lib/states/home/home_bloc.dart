9263ad46d678de0c985365b2b2e98efd0e6a7da8
@chamindu.etcetera <it19079264@my.sliit.lk>
2022-03-28 21:12:03 +0530

    create active tab

--- lib/states/home/home_bloc.dart
+++ lib/states/home/home_bloc.dart
@@ -1,28 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:chat/chat.dart';
-
 import 'home_state.dart';
 
 class HomeBloc extends Cubit<HomeState> {
   IUserService _userService;
-  ILocalCache _localCache;
 
-  HomeBloc(this._userService, this._localCache) : super(HomeInitial());
+  HomeBloc(this._userService) : super(HomeInitial());
 
-  Future<User> connect() async {
-    final userJson = _localCache.fetch('USER');
-    userJson['last_seen'] = DateTime.now();
-    userJson['active'] = true;
+  // Future<User> connect() async {
+  //   final userJson = _localCache.fetch('USER');
+  //   userJson['last_seen'] = DateTime.now();
+  //   userJson['active'] = true;
+  //
+  //   final user = User.fromJson(userJson);
+  //   await _userService.connect(user);
+  //   return user;
+  // }
 
-    final user = User.fromJson(userJson);
-    await _userService.connect(user);
-    return user;
-  }
-
-  Future<void> activeUsers(User user) async {
+  Future<void> activeUsers() async {
     emit(HomeLoading());
     final users = await _userService.online();
-    users.removeWhere((element) => element.id == user.id);
     emit(HomeSuccess(users));
   }
 }
