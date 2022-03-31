f0bf7896f657d44bd205b01811781cc2d87b8ffc
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-03-31 11:52:26 +0530

    UPDATE: static path added as origin in image url

--- lib/data/services/image_uploader_service.dart
+++ lib/data/services/image_uploader_service.dart
@@ -17,8 +17,7 @@ class ImageUploaderService {
     }
 
     final Response response = await Response.fromStream(result);
-    print(response.body);
 
-    return Uri.parse(_imageUrl).origin + response.body;
+    return 'http://10.0.2.2:3000' + response.body;
   }
 }
