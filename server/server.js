9668ec9ded57f1cc3686767c93fa1c36f2b7fcbf
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-03-31 07:08:02 +0530

    CREATE: get method added to root route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,10 @@ app.post("/upload", (req, res) => {
 	});
 });
 
+app.get("/", (req, res) => {
+	res.send("Hello from image server");
+});
+
 app.listen(3000, () => {
 	console.log("Server up and running on port " + 3000);
 });
