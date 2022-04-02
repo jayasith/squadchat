d33c4bec9b08bca8bc824c972b5bf743edca2217
hasansin <63632399+hasansin@users.noreply.github.com>
2022-04-02 22:46:35 +0530

    Updated login_cubit with localJson. Login router added to screens/login

---
+++ lib/views/screens/login/login_router.dart
@@ -0,0 +1,5 @@
+import 'package:squadchat/views/screens/login/login_router_contract.dart';
+
+class LoginRouter implements ILoginRouter{
+
+}
---
+++ lib/views/screens/login/login_router_contract.dart
@@ -0,0 +1,3 @@
+abstract class ILoginRouter {
+  void OnSessionSuccess(BuildContext context,User user);
+}
