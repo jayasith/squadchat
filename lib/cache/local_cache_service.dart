44e05f46d4f0c00b240aade4219c24cff63c5017
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-04-02 13:24:18 +0530

    CREATE : shared preferences services created

---
+++ lib/cache/local_cache_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:squadchat/cache/local_cache_contract.dart';
+
+class LocalCache implements ILocalCache {
+  final SharedPreferences _sharedPreferences;
+
+  LocalCache(this._sharedPreferences);
+
+  @override
+  Map<String, dynamic> fetch(String key) {
+    return jsonDecode(_sharedPreferences.getString(key) ?? '{}');
+  }
+
+  @override
+  Future<void> save(String key, Map<String, dynamic> json) async {
+    await _sharedPreferences.setString(key, jsonEncode(json));
+  }
+}
